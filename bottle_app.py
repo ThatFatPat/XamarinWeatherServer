548590e5b20f55c77cb281db0a4bad5da24ab275
ThatFatPat <idoshav@gmail.com>
2019-09-13 15:25:59 +0300

    Add github webhook

--- bottle_app.py
+++ bottle_app.py
@@ -1,6 +1,18 @@
 
 # A very simple Bottle Hello World app for you to get started with...
 from bottle import default_app, route
+import git
+
+
+@route('/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('.')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 @route('/')
 def hello_world():
