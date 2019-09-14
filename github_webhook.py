4f59686b0663fba78d67817c7f836d3915e34b62
ThatFatPat <idoshav@gmail.com>
2019-09-14 19:57:49 +0300

    Disable update_server when not hosted online (because of hard coded paths)

--- github_webhook.py
+++ github_webhook.py
@@ -4,6 +4,7 @@ import hmac
 import git
 import os
 import logging
+from socket import gethostname
 
 def is_valid_signature(x_hub_signature, data, private_key):
     hash_algorithm, github_signature = x_hub_signature.split('=', 1)
@@ -13,23 +14,18 @@ def is_valid_signature(x_hub_signature, data, private_key):
     return hmac.compare_digest(mac.hexdigest(), github_signature)
 
 def handle_pull():
+    if 'liveconsole' not in gethostname():
+        return 'Not on PythonAnywhere', 400
     x_hub_signature = request.headers.get('X-Hub-Signature')
     WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET")
-    logging.error(WEBHOOK_SECRET)
     GIT_FOLDER = "/home/ThatFatPat/xamarinweatherservice"
     logging.error("defined folder")
     if not is_valid_signature(x_hub_signature, request.data, WEBHOOK_SECRET):
-        logging.error("wrong key")
         return 'Invalid signature', 403
     if request.method == 'POST':
-        logging.error("in if")
         repo = git.Repo(GIT_FOLDER)
-        logging.error("found repo")
         origin = repo.remotes.origin
         origin.pull()
-        logging.error("past pull")
         return 'Updated PythonAnywhere successfully', 200
     else:
-        logging.error("400")
         return 'Wrong event type', 400
-    logging.error("WAT")
